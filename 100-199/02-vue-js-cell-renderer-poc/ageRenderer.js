7b3fdd1347474fc194ee9a695785c330668fa472
AhmedAGadir <ahmed.gadir@gmail.com>
2019-01-30 11:11:12 +0000

    update

---
+++ 100-199/02-vue-js-cell-renderer-poc/ageRenderer.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+
+export default Vue.extend({
+    template: `
+        <div>
+            <button v-on:click="randomAge()">
+                Randomise
+            </button>
+            {{this.value}}
+        </div>
+    `,
+    data: function () {
+        return {
+            value: null
+        };
+    },
+    beforeMount() {
+    },
+    mounted() {
+        console.log('mounted')
+        this.value = this.params.value;
+    },
+    methods: {
+        randomAge() {
+            var newValue = 13 + Math.floor(Math.random() * 15);
+            this.params.node.setDataValue('age', newValue);
+            // updates UI but doesnt update grid data 
+            // this.value = newValue;
+        }
+    }
+});
---
+++ 100-199/02-vue-js-cell-renderer-poc/main.js
@@ -0,0 +1,82 @@
+import Vue from "vue";
+import { AgGridVue } from "ag-grid-vue";
+
+import AgeRenderer from "./ageRenderer.js";
+
+const VueExample = {
+  template: `
+        <div style="height: 100%">
+            <div style="height: 100%; box-sizing: border-box;">
+                      <ag-grid-vue style="width: 100%; height: 100%;" class="ag-theme-balham" id="myGrid"
+                           :gridOptions="gridOptions"
+                           @grid-ready="onGridReady"
+                           :columnDefs="columnDefs"
+                          :rowData="rowData"
+                          :context="context"
+                          :rowHeight="rowHeight"></ag-grid-vue>
+            </div>
+        </div>
+    `,
+  components: {
+    "ag-grid-vue": AgGridVue
+  },
+  data: function () {
+    return {
+      gridOptions: null,
+      gridApi: null,
+      columnApi: null,
+      columnDefs: null,
+      rowData: null,
+      context: null,
+    };
+  },
+  beforeMount() {
+    this.gridOptions = {};
+    this.columnDefs = [
+      {
+        headerName: "Athlete",
+        field: "athlete"
+      },
+      {
+        headerName: "Age",
+        field: "age",
+        colId: 'age',
+        cellRendererFramework: AgeRenderer
+      }
+    ];
+    this.rowData = null;
+    this.context = { componentParent: this };
+    this.rowHeight = 50;
+  },
+  mounted() {
+    this.gridApi = this.gridOptions.api;
+    this.gridColumnApi = this.gridOptions.columnApi;
+  },
+  methods: {
+    onGridReady(params) {
+      const httpRequest = new XMLHttpRequest();
+      const updateData = data => {
+        this.rowData = data;
+      };
+
+      httpRequest.open(
+        "GET",
+        "https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinnersSmall.json"
+      );
+      httpRequest.send();
+      httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+          updateData(JSON.parse(httpRequest.responseText));
+        }
+      };
+      params.api.sizeColumnsToFit();
+    }
+  }
+};
+
+new Vue({
+  el: "#app",
+  components: {
+    "my-component": VueExample
+  }
+});
