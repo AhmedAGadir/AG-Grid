0dded0ebdfcf94c930bea210966bf6329fd01f4f
AhmedAGadir <ahmed.gadir@gmail.com>
2019-02-04 09:59:34 +0000

    update

---
+++ 100-199/07-pinned-row=data-agAnimateShowChangeCellRenderer-POC/main.js
@@ -0,0 +1,30 @@
+var gridOptions = {
+    columnDefs: [
+        { headerName: 'Athlete', field: 'athlete' },
+        { headerName: 'Sport', field: 'sport' },
+        { headerName: 'Age', field: 'age' },
+        { headerName: 'Year', field: 'year' },
+        { headerName: 'Date', field: 'date' },
+        { headerName: 'Gold', field: 'gold' },
+        { headerName: 'Silver', field: 'silver' },
+        { headerName: 'Bronze', field: 'bronze' },
+    ],
+    getRowStyle: params => params.node.rowPinned ? { backgroundColor: 'skyblue' } : null,
+    rowSelection: 'multiple',
+};
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+    var gridDiv = document.querySelector('#myGrid');
+    new agGrid.Grid(gridDiv, gridOptions);
+
+    agGrid
+        .simpleHttpRequest({
+            url:
+                'https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinnersSmall.json',
+        })
+        .then(function (data) {
+            gridOptions.api.setPinnedTopRowData(data.slice(0, 5))
+            gridOptions.api.setRowData(data.slice(5));
+        });
+});
