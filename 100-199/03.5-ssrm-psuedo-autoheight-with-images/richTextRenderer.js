d4897e09d2f1e0c5db314a58f88f496ad9fe535b
AhmedAGadir <ahmed.gadir@gmail.com>
2019-02-06 16:27:27 +0000

    update

---
+++ 100-199/03.5-ssrm-psuedo-autoheight-with-images/richTextRenderer.js
@@ -0,0 +1,35 @@
+function RichTextRenderer() { }
+
+RichTextRenderer.prototype.init = function (params) {
+    var offset = 120;
+    var img = document.createElement('img');
+    img.src = params.data.imageUrl;
+    if (params.data.rowHeight == null) {
+        console.log('adding img tag to DOM for first time')
+        img.addEventListener('load', () => {
+            let rowHeight = img.height + offset;
+            params.data.rowHeight = rowHeight;
+            params.onImageLoaded(params.node, img.height + offset);
+        })
+    }
+
+
+    this.eGui = document.createElement('div');
+    this.eGui.innerHTML = getRichText();
+    this.eGui.appendChild(img);
+}
+
+RichTextRenderer.prototype.getGui = function () {
+    return this.eGui;
+}
+
+function getRichText() {
+    const str = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum deserunt maiores nobis, tempora repellat libero asperiores blanditiis dignissimos dolore dolorem excepturi perspiciatis. Necessitatibus officiis error nisi veritatis delectus, ipsum et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum deserunt maiores nobis, tempora repellat libero asperiores blanditiis dignissimos dolore dolorem excepturi perspiciatis. Necessitatibus officiis error nisi veritatis delectus, ipsum et.';
+    const randInd = Math.floor(Math.random() * str.length);
+    const richText = `
+    <div style="white-space: normal !important">
+        ${str.slice(0, randInd)}
+    </div>
+  `
+    return richText;
+}
