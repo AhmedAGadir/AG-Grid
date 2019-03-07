0ff260571163ba6cb559f8898c790666a92b4075
AhmedAGadir <ahmed.gadir@gmail.com>
2019-03-07 11:23:28 +0000

    update

--- 100-199/37.5-redux-find-out-which-row-is-being-updated/src/App.js
+++ 100-199/37.5-redux-find-out-which-row-is-being-updated/src/App.js
@@ -71,7 +71,7 @@ class App extends Component {
             columnDefs={[
               { headerName: 'Athlete', field: 'athlete' },
               { headerName: 'Age', field: 'age' },
-              { headerName: 'Number Of Times Updates', field: 'updates', width: 200 }
+              { headerName: 'Number Of Times Updated', field: 'updates', width: 200 }
             ]}
             rowData={this.props.rowData}
             defaultColDef={{ width: 150, editable: true }}
