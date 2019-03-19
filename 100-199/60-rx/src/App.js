f8a2299ad073783a463b80a181903eb4329fac33
AhmedAGadir <ahmed.gadir@gmail.com>
2019-03-19 13:59:45 +0000

    update

---
+++ 100-199/60-rx/src/App.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import 'ag-grid-enterprise';
+
+import { connect } from 'react-redux';
+import * as actions from './store/actions';
+import uuidv4 from 'uuid';
+
+class App extends Component {
+
+  componentDidMount() {
+    fetch('https://raw.githubusercontent.com/ag-grid/ag-grid-docs/latest/src/javascript-grid-master-detail/simple/data/data.json')
+      .then(res => res.json())
+      .then(data => {
+        data.forEach(d => d.id = uuidv4())
+        this.props.onInitRowData(data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    this.gridApi.sizeColumnsToFit();
+
+    setTimeout(() => {
+      var rowCount = 0;
+      this.gridApi.forEachNode(function (node) {
+        node.setExpanded(rowCount++ === 1);
+      });
+    }, 500);
+  }
+
+  render() {
+    return (
+      <div
+        className="ag-theme-balham"
+        style={{
+          height: '90vh',
+          // width: '100%'
+        }}>
+        <AgGridReact
+          columnDefs={[
+            // group cell renderer needed for expand / collapse icons
+            { field: 'name', cellRenderer: 'agGroupCellRenderer' },
+            { field: 'account' },
+            { field: 'calls' },
+            { field: 'minutes', valueFormatter: "x.toLocaleString() + 'm'" }
+          ]}
+          masterDetail={true}
+          detailCellRendererParams={{
+            detailGridOptions: {
+              columnDefs: [
+                { field: 'callId' },
+                { field: 'direction' },
+                { field: 'number' },
+                { field: 'duration', valueFormatter: "x.toLocaleString() + 's'" },
+                { field: 'switchCode' }
+              ],
+              onFirstDataRendered(params) {
+                params.api.sizeColumnsToFit();
+              }
+            },
+            getDetailRowData: function (params) {
+              params.successCallback(params.data.callRecords);
+            }
+          }}
+          rowData={this.props.rowData}
+          defaultColDef={{ width: 150 }}
+          onGridReady={this.onGridReady.bind(this)}
+          deltaRowDataMode={true}
+          getRowNodeId={data => data.id}>
+        </AgGridReact>
+      </div >
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    rowData: state.rowData
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onInitRowData: rowData => dispatch(actions.initRowData(rowData)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
