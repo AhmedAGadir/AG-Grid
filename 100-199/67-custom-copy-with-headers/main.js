9d2eefc8f3e30eaef4835544d858cc829bf33e0b
AhmedAGadir <ahmed.gadir@gmail.com>
2019-03-27 16:41:37 +0000

    update

--- 100-199/67-custom-copy-with-headers/main.js
+++ 100-199/67-custom-copy-with-headers/main.js
@@ -1,36 +1,56 @@
+// var columnDefs = [
+//     { headerName: 'Athlete', field: 'athlete', width: 150 },
+//     { headerName: 'Age', field: 'age', width: 90 },
+//     { headerName: 'Country', field: 'country', width: 120 },
+//     { headerName: 'Year', field: 'year', width: 90, },
+//     { headerName: 'Date', field: 'date', width: 110 },
+//     { headerName: 'Sport', field: 'sport', width: 110 },
+//     { headerName: 'Gold', field: 'gold', width: 100 },
+//     { headerName: 'Silver', field: 'silver', width: 100 },
+//     { headerName: 'Bronze', field: 'bronze', width: 100 },
+//     { headerName: 'Total', field: 'total', width: 100 }
+// ];
+
 var columnDefs = [
-    {headerName: 'Athlete', field: 'athlete', width: 150},
-    {headerName: 'Age', field: 'age', width: 90},
-    {headerName: 'Country', field: 'country', width: 120},
-    {headerName: 'Year', field: 'year', width: 90},
-    {headerName: 'Date', field: 'date', width: 110},
-    {headerName: 'Sport', field: 'sport', width: 110},
-    {headerName: 'Gold', field: 'gold', width: 100},
-    {headerName: 'Silver', field: 'silver', width: 100},
-    {headerName: 'Bronze', field: 'bronze', width: 100},
-    {headerName: 'Total', field: 'total', width: 100}
+    { headerName: 'Athlete', field: 'athlete', width: 150 },
+    { headerName: 'Age', field: 'age', width: 90 },
+    { headerName: 'Country', field: 'country', width: 120, enableRowGroup: true, rowGroup: true },
+    { headerName: 'Year', field: 'year', width: 90, enablePivot: true, pivot: true },
+    { headerName: 'Date', field: 'date', width: 110 },
+    { headerName: 'Sport', field: 'sport', width: 110, enablePivot: true, pivot: true },
+    { headerName: 'Gold', field: 'gold', width: 100, enableValue: true, aggFunc: 'sum' },
+    { headerName: 'Silver', field: 'silver', width: 100, enableValue: true, aggFunc: 'sum' },
+    { headerName: 'Bronze', field: 'bronze', width: 100, enableValue: true, aggFunc: 'sum' },
 ];
 
 var gridOptions = {
     columnDefs: columnDefs,
     enableRangeSelection: true,
     getContextMenuItems: getContextMenuItems,
-    allowContextMenuWithControlKey: true
+    allowContextMenuWithControlKey: true,
+    sideBar: true,
+    pivotMode: true
 };
-
-function createFlagImg(flag) {
-    return '<img border="0" width="15" height="10" src="https://flags.fmcdn.net/data/flags/mini/' + flag + '.png"/>';
-}
 
 function getContextMenuItems(params) {
     var result = [
         {
             // custom item
             name: 'Copy With Headers',
-            action: function() {
-                debugger;
+            action: function () {
+                if (params.columnApi.isPivotMode()) {
+                    var pivotKeysStr = params.column.colDef.pivotKeys.join('\n');
+                    var aggregation = params.column.colDef.aggFunc + '(' + params.column.colDef.headerName + ')'
+                    var cellContent = (params.value !== null && params.value !== undefined && params.value !== '') ? params.value : '\n';
+
+                    var result = pivotKeysStr + '\n' + aggregation + '\n' + cellContent;
+                    params.api.clipboardService.copyDataToClipboard(result);
+                } else {
+                    params.api.clipboardService.copyToClipboard(true)
+                }
             },
         },
+        // 'copyWithHeaders',
         'copy'
     ];
 
@@ -38,7 +58,7 @@ function getContextMenuItems(params) {
 }
 
 // setup the grid after the page has finished loading
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var gridDiv = document.querySelector('#myGrid');
     new agGrid.Grid(gridDiv, gridOptions);
 
@@ -47,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var httpRequest = new XMLHttpRequest();
     httpRequest.open('GET', 'https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinners.json');
     httpRequest.send();
-    httpRequest.onreadystatechange = function() {
+    httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
             var httpResult = JSON.parse(httpRequest.responseText);
             gridOptions.api.setRowData(httpResult);
