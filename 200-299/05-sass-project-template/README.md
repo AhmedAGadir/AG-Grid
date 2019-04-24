9f0f4354a175d999220c932ce10cc3e320148e1c
AhmedAGadir <ahmed.gadir@gmail.com>
2019-04-24 11:05:28 +0100

    update

---
+++ 200-299/05-sass-project-template/README.md
@@ -0,0 +1,14 @@
+# ag-Grid Theme Customization Example
+
+The project in this repository showcases how the ag-Grid built-in themes can be customized by overriding Sass variables. 
+An up-to-date table with the variable default values and their function is available at: http://www.ag-grid.com/javascript-grid-styling/#customizing-sass-variables
+
+## Usage
+
+- Clone the project
+- run `npm install`
+- start with `npm start`
+- Examine the [`styles.scss`](src/styles/styles.scss) file for details
+- Access [`http://localhost:8081`](http://localhost:8081)
+
+Note: This project is using custom icons from https://fontawesome.com/
---
+++ 200-299/05-sass-project-template/src/grid.js
@@ -0,0 +1,39 @@
+
+import * as agGrid from 'ag-grid-community';
+import 'ag-grid-enterprise';
+import './styles/styles.scss';
+
+
+var gridOptions = {
+    columnDefs: [
+        { headerName: 'Athlete', field: 'athlete' },
+        { headerName: 'Country', field: 'country' },
+        { headerName: 'Sport', field: 'sport' },
+        { headerName: 'Age', field: 'age' },
+        { headerName: 'Year', field: 'year' },
+        { headerName: 'Date', field: 'date' },
+        { headerName: 'Gold', field: 'gold' },
+        { headerName: 'Silver', field: 'silver' },
+        { headerName: 'Bronze', field: 'bronze' },
+        { headerName: 'Total', field: 'total' }
+    ],
+    defaultColDef: {
+        width: 150,
+        filter: true
+    },
+    rowData: null,
+    headerHeight: 50,
+    rowHeight: 50,
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    var gridDiv = document.querySelector('#myGrid');
+    new agGrid.Grid(gridDiv, gridOptions);
+    agGrid.simpleHttpRequest({
+        url:
+            'https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinnersSmall.json',
+    })
+        .then(function (data) {
+            gridOptions.api.setRowData(data);
+        });
+}); 
