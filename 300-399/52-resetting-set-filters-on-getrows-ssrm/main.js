168351004d14c2e31255a96048124900e8978020
AhmedAGadir <ahmed.gadir@gmail.com>
2020-10-02 15:47:04 +0100

    update

--- 300-399/52-resetting-set-filters-on-getrows-ssrm/main.js
+++ 300-399/52-resetting-set-filters-on-getrows-ssrm/main.js
@@ -1,10 +1,8 @@
-const delay = ms => new Promise(res => setTimeout(res, ms));
-
 // button click handler
 async function setCountryFilters() {
   console.log('setting country filter values');
   var instance = gridOptions.api.getFilterInstance('country');
-  let values = await getUpdatedCountryValuesAsync();
+  let values = await getCountryValuesAsync();
   instance.setFilterValues(values);
   gridOptions.api.onFilterChanged();
 }
@@ -13,18 +11,21 @@ async function setCountryFilters() {
 async function setSportFilters() {
   console.log('setting sport filter values');
   var instance = gridOptions.api.getFilterInstance('sport');
-  let values = await getUpdatedSportValuesAsync();
+  let values = await getSportValuesAsync();
   instance.setFilterValues(values);
   gridOptions.api.onFilterChanged();
 }
 
 var columnDefs = [
-  {cellRenderer: params => params.node.rowIndex},
+  { cellRenderer: params => params.node.rowIndex, maxWidth: 60 },
   {
     field: 'country',
     filter: 'agSetColumnFilter',
     filterParams: {
-      values: getCountryValuesAsync,
+      values: async params => {
+        let values = await getCountryValuesAsync();
+        return params.success(values);
+      },
     },
     menuTabs: ['filterMenuTab'],
   },
@@ -32,7 +33,10 @@ var columnDefs = [
     field: 'sport',
     filter: 'agSetColumnFilter',
     filterParams: {
-      values: getSportValuesAsync,
+      values: async params => {
+        let values = await getSportValuesAsync();
+        return params.success(values);
+      },
     },
     menuTabs: ['filterMenuTab'],
   },
@@ -87,36 +91,18 @@ var selectedCountries = null;
 //   );
 // }
 
-// for getting initial country filter values
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+// for getting country filter values
 async function getCountryValuesAsync(params) {
   var countries = fakeServer.getCountries();
-
-  await delay(500);
-  params.success(countries);
-}
-
-// for getting initial sport filter values
-async function getSportValuesAsync(params) {
-  var sports = fakeServer.getSports(selectedCountries);
-
-  await delay(500);
-  params.success(sports);
-}
-
-// for getting updated country filter values
-async function getUpdatedCountryValuesAsync(params) {
-  var countries = fakeServer.getCountries();
-  countries = ['Sudan'];
-
   await delay(500);
   return countries;
 }
 
-// for getting updated sport filter values
-async function getUpdatedSportValuesAsync(params) {
+// for getting sport filter values
+async function getSportValuesAsync(params) {
   var sports = fakeServer.getSports(selectedCountries);
-  sports = ['BJJ'];
-
   await delay(500);
   return sports;
 }
@@ -126,7 +112,7 @@ function ServerSideDatasource(server) {
   this.resettingFilterValues = false;
 }
 
-ServerSideDatasource.prototype.getRows = function(params) {
+ServerSideDatasource.prototype.getRows = function (params) {
   console.log('[Datasource] - rows requested by grid: ', params.request);
   console.log('resettingfiltervalues', this.resettingFilterValues);
 
@@ -134,39 +120,39 @@ ServerSideDatasource.prototype.getRows = function(params) {
 
   // simulating real server call with a 500ms delay
   setTimeout(async () => {
-      if (response.success) {
-        params.successCallback(response.rows, response.lastRow);
-      } else {
-        params.failCallback();
-      }
+    if (response.success) {
+      params.successCallback(response.rows, response.lastRow);
+    } else {
+      params.failCallback();
+    }
 
-      if (this.resettingFilterValues) {
-        // prevents infinite loop
-        this.resettingFilterValues = false;
-        return;
-      } else {
-        // reset the filters after each getRows call
-        this.resettingFilterValues = true;
-        this.resetFilterValues();
-      }
+    if (this.resettingFilterValues) {
+      // prevents infinite loop
+      this.resettingFilterValues = false;
+      return;
+    } else {
+      // reset the filters after each getRows call
+      this.resettingFilterValues = true;
+      this.resetFilterValues();
+    }
   }, 500);
 
 };
 
-ServerSideDatasource.prototype.resetFilterValues = async function() {
+ServerSideDatasource.prototype.resetFilterValues = async function () {
   // get scroll position
   // let topIndex = gridOptions.api.getFirstDisplayedRow();
   // console.log('topIndex', topIndex);
 
   // set country filters
-  var instance = gridOptions.api.getFilterInstance('country');
-  let countryValues = await getUpdatedCountryValuesAsync();
-  instance.setFilterValues(countryValues);
+  var countryFilterInstance = gridOptions.api.getFilterInstance('country');
+  let countryValues = await getCountryValuesAsync();
+  countryFilterInstance.setFilterValues(countryValues);
 
   // set sports filters
-  var instance = gridOptions.api.getFilterInstance('sport');
-  let sportValues = await getUpdatedSportValuesAsync();
-  instance.setFilterValues(sportValues);
+  var sportsFilterInstance = gridOptions.api.getFilterInstance('sport');
+  let sportValues = await getSportValuesAsync();
+  sportsFilterInstance.setFilterValues(sportValues);
 
   // update
   console.log('onFilterChanged');
@@ -181,7 +167,7 @@ ServerSideDatasource.prototype.resetFilterValues = async function() {
 };
 
 // setup the grid after the page has finished loading
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var gridDiv = document.querySelector('#myGrid');
   new agGrid.Grid(gridDiv, gridOptions);
 
@@ -190,7 +176,7 @@ document.addEventListener('DOMContentLoaded', function() {
       url:
         'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/olympicWinners.json',
     })
-    .then(function(data) {
+    .then(function (data) {
       // setup the fake server with entire dataset
       fakeServer = new FakeServer(data);
 
